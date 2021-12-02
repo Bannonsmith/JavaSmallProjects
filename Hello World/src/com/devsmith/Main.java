6a80bf33e1274e5691d92ccbd3601e0f6ea58a10
Bannon smith <Bannonsmith13@gmail.com>
2021-12-01 23:49:39 -0600

    Strings

--- Hello World/src/com/devsmith/Main.java
+++ Hello World/src/com/devsmith/Main.java
@@ -2,17 +2,31 @@ package com.devsmith;
 
 
 
-import java.awt.*;
+
 
 
 public class Main {
 
     public static void main(String[] args) {
+//        Strings
+        String message = "   Hello World   " + "!!    ";
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.length());
+        System.out.println(message.indexOf("H"));
+        System.out.println(message.replace("!", "*"));
+        System.out.println(message.toLowerCase());
+        System.out.println(message.toUpperCase());
+        System.out.println(message.trim());
+
+//        You can also do this to declare the String but not needed
+//        String message = new String("Hello World");
+        System.out.println(message);
+
 //        PRIMITVESTYPES VS REFERENCE TYPES
-        Point point1 = new Point( x:1, y:1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+//        Point point1 = new Point( x:1, y:1);
+//        Point point2 = point1;
+//        point1.x = 2;
+//        System.out.println(point2);
 
 
 //        byte x = 1;
