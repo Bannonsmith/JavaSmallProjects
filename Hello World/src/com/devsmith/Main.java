e903a68858e3a12f5c88a8382a0f8a4df48ada96
Bannon smith <Bannonsmith13@gmail.com>
2021-12-01 23:11:32 -0600

    primitives

---
+++ Hello World/src/com/devsmith/Main.java
@@ -0,0 +1,13 @@
+package com.devsmith;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte age = 30;
+        long viewsCount = 3123456789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+        System.out.println(age);
+    }
+}
